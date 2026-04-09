ad4665be007183b7726dc1444eec8b27aa6ce1eb
fr05ha <mikey.frolkin@gmail.com>
2026-04-09 20:37:40 +1000

    Add files via upload

--- test-calculator.py
+++ test-calculator.py
@@ -1,1 +1,11 @@
-from calculator.py import add, subtract, multiplay
+from calculator import add, subtract, multiply
+
+
+def test_add():
+    assert add(2 ,3)==5
+
+def test_subtract():
+    assert subtract(10, 3) == 7
+
+def test_multiply():
+    assert multiply(4, 3)== 12
